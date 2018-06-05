e957aa00a10d723d5e6cafecff512b5b21e177ba
Guillem <guillemeetac@gmail.com>
2018-06-05 10:41:34 +0200

    Test for velocity

--- test.py
+++ test.py
@@ -1,9 +1,30 @@
+import math
+import numpy as np
 from AirSimClient import *
 
-# connect to the AirSim simulator 
-client = CarClient()
+
+client = MultirotorClient()
 client.confirmConnection()
 client.enableApiControl(True)
-car_controls = CarControls()
-car_state = client.getCarState()
-print (car_state.kinematics_true)
+client.armDisarm(True)
+#client.takeoff()
+#client.hover()
+client.moveToZ(-6, 5)
+duration = 5
+speed_x = -3
+speed_y= 3
+
+pitch, roll, yaw  = client.getPitchRollYaw()
+vel = client.getVelocity()
+vx = math.cos(yaw) * speed_x - math.sin(yaw) * speed_y
+vy = math.sin(yaw) * speed_x + math.cos(yaw) * speed_y
+
+drivetrain = DrivetrainType.ForwardOnly
+yaw_mode = YawMode(is_rate= False, yaw_or_rate = 0)
+
+client.moveByVelocityZ(vx = (vx +vel.x_val)/2 ,
+                       vy = (vy +vel.y_val)/2 , #do this to try and smooth the movement
+                       z = -6,
+                       duration = duration + 5,
+                       drivetrain = drivetrain,
+                       yaw_mode = yaw_mode)
